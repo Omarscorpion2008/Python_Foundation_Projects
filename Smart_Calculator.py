54c2744a6fefde003bcd6a3a492c01e6dbe7dce4
Omar Anas <elmahdyomar32@gmail.com>
2025-12-20 14:09:16 +0300

    base class created

---
+++ Smart_Calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+    
+    def input(self):
+        pass
+
+    def parse(self, question):
+        pass
+
+    def evalutaion(self):
+        pass
+
+    def output(self):
+        pass
+    
