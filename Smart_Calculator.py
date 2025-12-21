bd99323ca0f0729002b8f31c8429b2c8587d293d
Omar Anas <elmahdyomar32@gmail.com>
2025-12-21 14:07:47 +0300

    parsing def created

--- Smart_Calculator.py
+++ Smart_Calculator.py
@@ -1,6 +1,7 @@
 class Calculator:
 
     def __init__(self):
+        self.question = ''
         self.numbers = []
         self.spaces = []
         self.operations = []
@@ -8,10 +9,29 @@ class Calculator:
         self.buffer = ''
     
     def input(self):
-        return input("Enter your Expression: ")
+        question = input("Enter your Expression: ")
 
-    def parse(self):
-        pass
+    def parse(self, question, operations, numbers):
+
+        for character in question:
+
+            if question[0] and character == '-':
+                buffer = buffer + str(character)
+        
+            elif character.isnumeric()  or character == '.':
+                buffer = buffer + str(character)
+        
+            elif character in '+-*/':
+                if buffer != '':
+                    numbers.append(buffer)
+                    buffer = ''
+                operations.append(character)
+        
+            elif character == ' ':
+                if buffer != '':
+                    numbers.append(buffer)
+                    buffer = ''
+        numbers.append(buffer)
 
     def evalutaion(self):
         pass
