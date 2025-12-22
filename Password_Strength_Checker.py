aca8490c853dea57fa78ce7759d81e7b4259ba5c
Omar Anas <elmahdyomar32@gmail.com>
2025-12-22 11:35:44 +0300

    main class created

---
+++ Password_Strength_Checker.py
@@ -0,0 +1,10 @@
+class password_checker:
+    def __init__(self):
+        self.password = ''
+        self.score = 0
+    
+    def input(self):
+    
+    def evaluations(self):
+
+    def output(self):
