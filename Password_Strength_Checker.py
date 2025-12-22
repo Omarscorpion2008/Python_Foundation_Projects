80f5ab59367f8963b8272cb43d49d809c2ddf3e1
Omar Anas <elmahdyomar32@gmail.com>
2025-12-22 17:59:07 +0300

    fixes in the evaluation def

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -1,43 +1,50 @@
 class password_checker:
     def __init__(self):
         self.password = ''
-        self.score = 0
+        self.buffer = ''
+        self.score = 0.0
     
     def input(self):
         self.password = input("Enter a Password: ")
-        
+
     def evaluations(self):
 
-        if len(self.password) < 12:
+        if all(c.isspace() for c in self.password) or self.password == '':
             self.score = 0
-            return self.score
-        elif len(self.password) >= 14:
+
+        if len(self.password) < 14:
+            self.score = 0
+
+        if len(self.password) >= 14:
             self.score = self.score + 4
             if len(self.password) > 14:
                 bonus = (len(self.password) - 14) / 2
                 self.score = self.score + bonus
-        elif all(c.isspace() for c in self.password) or self.password == '':
-            self.score = 0
-        elif not any(c.isalpha() for c in self.password):
-            self.score = min(self.score, 4)
-        elif any(c.islower() for c in self.password):
+        
+        if any(c.islower() for c in self.password):
             self.score = self.score + 2
-        elif any(c.isupper() for c in self.password):
+        
+        if any(c.isupper() for c in self.password):
             self.score = self.score + 2
-        elif any(c.isnumeric() for c in self.password):
+        
+        if any(c.isnumeric() for c in self.password):
             self.score = self.score + 2
-        elif any(c in '!@#$%^&*.' for c in self.password):
+        
+        if any(c in '!@#$%^&*.' for c in self.password):
             self.score = self.score + 3
+
+        if not any(c.isalpha() for c in self.password):
+            self.score = min(self.score, 4)
 
     def output(self):
 
-        if self.score <= 3:
+        if self.score <= 4:
             print(f"Weak Password: {self.score}")
-        elif self.score >= 4 and self.score <= 6:
+        elif self.score >= 5 and self.score <= 10:
             print(f"Medium Password: {self.score}")
-        elif self.score >= 7 and self.score <= 9:
+        elif self.score >= 11 and self.score <= 20:
             print(f"Strong Password: {self.score}")
-        elif self.score >= 10:
+        elif self.score > 20:
             print(f"Very strong password: {self.score}")
 
 pass_checker = password_checker()
