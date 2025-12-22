b7e30063c289521fc5a97b8158c80e3079f97928
Omar Anas <elmahdyomar32@gmail.com>
2025-12-22 14:04:06 +0300

    base checker done, fixes incoming

--- Password_Strength_Checker.py
+++ Password_Strength_Checker.py
@@ -7,9 +7,30 @@ class password_checker:
         self.password = input("Enter a Password: ")
         
     def evaluations(self):
-        pass
 
+        if len(self.password) < 12:
+            self.score = 0
+        elif len(self.password) >= 14:
+            self.score = self.score + 4
+            if len(self.password) > 14:
+                bonus = (len(self.password) - 14) / 2
+                self.score = self.score + bonus
+
+        if not any(c.isalpha() for c in self.password):
+            self.score = min(self.score, 4)
+        elif all(c.isspace() for c in self.password) or self.password = '':
+            self.score = 0
+        
+        if any(c.islower() for c in self.password):
+            self.score = self.score + 2
+        elif any(c.isupper() for c in self.password):
+            self.score = self.score + 2
+        elif any(c.isnumeric() for c in self.password):
+            self.score = self.score + 2
+        elif any(c in '!@#$%^&*.' for c in self.password):
+            self.score = self.score + 3
     def output(self):
+
         if self.score <= 3:
             print(f"Weak Password: {self.score}")
         elif self.score >= 4 or self.score <= 6:
