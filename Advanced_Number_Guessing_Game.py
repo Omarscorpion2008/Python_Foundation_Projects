e01416e513a38d523e34fe62f966dfd472b5237b
Omarscorpion2008 <elmahdyomar32@gmail.com>
2025-12-24 11:59:16 +0300

    Advanced Number Guessing Game project started

---
+++ Advanced_Number_Guessing_Game.py
@@ -0,0 +1,11 @@
+class Game:
+    def __init__(self):
+        self.high_score = 0
+        self.current_score = 0
+        self.attempts = 0
+    
+    def guessing_machine(self):
+        pass
+    
+    def output(self):
+        pass
