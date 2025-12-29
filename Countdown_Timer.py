3e285c5e4680b6e2d6d3dd9f4c5c3ddb1aa92ea7
Omarscorpion2008 <elmahdyomar32@gmail.com>
2025-12-29 14:47:22 +0300

    first prototype

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -6,6 +6,19 @@ class Timer:
         self.hours = float(self.parts[0])
         self.minutes = float(self.parts[1])
         self.seconds = float(self.parts[2])
-    
+        
+        Timer.Time_Calculator(self)
     def Time_Calculator(self):
-        pass
+        self.hours = self.hours * 3600
+        self.minutes = self.minutes * 60
+        
+        self.total_time = self.hours + self.minutes + self.seconds
+        Timer.Time_Passing_mechanism(self)
+    def Time_Passing_mechanism(self):
+        while self.total_time > 0 :
+            print(time)
+            time.sleep(1)
+            self.total_time = self.total_time - 1
+
+countdown = Timer()
+countdown.user_input_classifier()
