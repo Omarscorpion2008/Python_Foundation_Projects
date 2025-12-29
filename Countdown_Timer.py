c7307277d7a91bff9ade4e25216e026503ae37cf
Omarscorpion2008 <elmahdyomar32@gmail.com>
2025-12-29 14:19:21 +0300

    base classifier def made

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -1,3 +1,11 @@
+import time
 class Timer:
-    def __init__(self):
-        self.user_input = input("Enter the time you want: ")
+    def user_input_classifier(self):
+        self.user_input = input("Enter the time you want [HH:MM:SS]: ")
+        self.parts = self.user_input.split(':')
+        self.hours = float(self.parts[0])
+        self.minutes = float(self.parts[1])
+        self.seconds = float(self.parts[2])
+    
+    def Time_Calculator(self):
+        pass
