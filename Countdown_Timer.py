5456799af39a3f7735af6d4ffc22499ee2c8aa23
Omar Anas <elmahdyomar32@gmail.com>
2025-12-30 12:03:15 +0300

    main countdown done, slight exceptions under construction.

--- Countdown_Timer.py
+++ Countdown_Timer.py
@@ -3,22 +3,36 @@ class Timer:
     def user_input_classifier(self):
         self.user_input = input("Enter the time you want [HH:MM:SS]: ")
         self.parts = self.user_input.split(':')
-        self.hours = float(self.parts[0])
-        self.minutes = float(self.parts[1])
-        self.seconds = float(self.parts[2])
+        self.hours = int(self.parts[0])
+        self.minutes = int(self.parts[1])
+        self.seconds = int(self.parts[2])
         
-        Timer.Time_Calculator(self)
+        if self.user_input != '':
+            Timer.Time_Calculator(self)
+        else:
+            print("Thanks for using the Countdown.")
+            exit()
+            
+
     def Time_Calculator(self):
         self.hours = self.hours * 3600
         self.minutes = self.minutes * 60
-        
         self.total_time = self.hours + self.minutes + self.seconds
         Timer.Time_Passing_mechanism(self)
+
     def Time_Passing_mechanism(self):
-        while self.total_time > 0 :
-            print(time)
+        for current_countdown in range(self.total_time, 0, -1):
+            self.hours = int((current_countdown / 60) / 60) % 60
+            self.minutes = int(current_countdown / 60) % 60
+            self.seconds = int(current_countdown % 60)
+            print(f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}")
             time.sleep(1)
-            self.total_time = self.total_time - 1
+        print("! TIME'S UP !")
 
 countdown = Timer()
-countdown.user_input_classifier()
+while True:
+    try:
+        countdown.user_input_classifier()
+    except (ValueError, TypeError, IndexError):
+        print("Please provide valid inputs.")
+        countdown.user_input_classifier()
