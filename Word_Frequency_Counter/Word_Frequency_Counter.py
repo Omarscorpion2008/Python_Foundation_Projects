a45b0f23da63706885f4b844b439c2a2f74f23e7
Omarscorpion2008 <elmahdyomar32@gmail.com>
2026-01-14 10:50:52 +0300

    parsing, counting and result printer completed.

--- Word_Frequency_Counter/Word_Frequency_Counter.py
+++ Word_Frequency_Counter/Word_Frequency_Counter.py
@@ -14,8 +14,17 @@ class Word_Frequency:
                 self.sentence = self.sentence + " "
             else:
                 self.sentence = self.sentence + character.lower()
+        self.parsing_counting()
         
+    def parsing_counting(self):
+        self.new_sentence = self.sentence.split(' ')
+        
+        for word in self.new_sentence:
+            if word:
+                self.dictionary[word] = self.dictionary.get(word, 0) + 1
 
-            
+        for item in self.dictionary:
+            print(f"{item}: {self.dictionary.get(item)}")
+        
 word = Word_Frequency()
 word.start()
