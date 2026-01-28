aff8a3b584d65f497d2ea22f00f41f60495d0e72
Omarscorpion2008 <elmahdyomar32@gmail.com>
2026-01-28 12:08:30 +0300

    minor change in ask def, score checker and output def added. End of project

--- Quiz_Game_Engine/Quiz_Game_Engine.py
+++ Quiz_Game_Engine/Quiz_Game_Engine.py
@@ -19,11 +19,35 @@ class Quiz:
         self.Ask()
         
     def Ask(self):
+        letters = ['A', 'B', 'C', 'D']
         for question in self.questions:
-            print(question)
-            print(self.questions[question])
-            user_input = input("Answer: ")
+            print("\n" + question)
+            for i, choice in enumerate(self.questions[question]):
+                print(f"{letters[i]}) {choice}")
+            user_input = input("Answer: ").upper()
             self.user_answers.append(user_input)
     
+        self.score_checker()
+
     
+    def score_checker(self):
+        for answer in range(len(self.user_answers)):
+            if list(self.answers.values())[answer] == self.user_answers[answer]:
+                print(f"Question Number {answer} was answered correctly !")
+                self.score = self.score + 1
+            else:
+                print(f"Question Number {answer} was answered wrongly :(")
+        self.output()
+    
+    def output(self):
+        self.total_question_count = len(list(self.questions))
+        score_percentage = ( self.score / len(list(self.questions)) ) * 100
+        
+        print("Quiz Complete!")
+        print(f"You scored {self.score} out of {self.total_question_count} ({score_percentage:.2f}%)")
+        
+        if score_percentage < 50:
+            print("\nYou certainly can do better bro")
+        else:
+            print("\nNice job!")
 Quiz()
