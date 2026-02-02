1050f115bb4e5c93928d33958da1c385db378451
Omarscorpion2008 <elmahdyomar32@gmail.com>
2026-02-02 09:49:48 +0300

    random data generators defs created

--- Random_Data_Generator/Random_Data_Generator.py
+++ Random_Data_Generator/Random_Data_Generator.py
@@ -1,3 +1,4 @@
+import random
 class Generator:
     def __init__(self):
         self.first_names = [
@@ -22,6 +23,40 @@ class Generator:
             "yahoo.com",
             "outlook.com",
             "hotmail.com"]
+        
+        self.names = []
+        self.ages = []
+        self.emails = []
+        
+        self.startup()
     
     def startup(self):
+        self.number_of_generated_lines = int(input("How many lines would you like to generate? : "))
+        self.fake_name_generator()
+        self.fake_age_generator()
+        self.fake_email_generator()
+        self.data_saver()
+    
+    def fake_name_generator(self):
+        for line in range(self.number_of_generated_lines):
+            first_name = random.choice(self.first_names)
+            last_name = random.choice(self.last_names)
+            name = str(first_name + ' ' + last_name)
+            self.names.append(name)
+    
+    def fake_age_generator(self):
+        for line in range(self.number_of_generated_lines):
+            age = random.randint(18,60)
+            self.ages.append(age)
+    
+    def fake_email_generator(self):
+        for line in range(self.number_of_generated_lines):
+            name = self.names[line].replace(' ', '').lower()
+            number = random.randint(0,3000)
+            domain = random.choice(self.email_domains)
+            email = str(name) + str(number) + '@' + str(domain)
+            self.emails.append(email)
+    
+    def data_saver(self):
         pass
+Generator()
