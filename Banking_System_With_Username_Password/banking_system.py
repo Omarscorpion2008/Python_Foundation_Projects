27f559ca89e1cdca3c1d81ac0599091d49139236
Omarscorpion2008 <elmahdyomar32@gmail.com>
2026-02-04 11:39:47 +0300

    better alignment with the enviroment, minor and major edits occured.

--- Banking_System_With_Username_Password/banking_system.py
+++ Banking_System_With_Username_Password/banking_system.py
@@ -2,7 +2,7 @@ class Bank:
     def __init__(self):
         self.database = {}
         self.dataloader()
-        self.validation()
+        self.main_menu()
         self.data_updater()
     
     def dataloader(self):
@@ -16,46 +16,85 @@ class Bank:
                 balance = int(items[2])
                 self.database[username] = [password, balance]
     
-    def validation(self):
-        self.username = input("Provide your username: ")
-        self.password = input("Provide your Password: ")    
-        while True:
-            if self.username in self.database.keys():
-                if self.database[self.username][0] == self.password:
-                    user_answer = input("Deposit (d) / Withdraw (w) / Balance Check (b) / exit (q): ").lower()
-                    if user_answer == 'd':
-                        self.deposit()
-                    elif user_answer == 'w':
-                        self.withdraw()
-                    elif user_answer == 'b':
-                        self.balance_check()
-                    else:
+    def main_menu(self):
+            print('-' * 30)
+            print("   -   Sign in window   - ")
+            print('-' * 30)
+            self.username = input("Please provide your username: ")
+            self.password = input("Please provide your password: ")
+            while True:
+                if self.username == '' and self.password == '':
+                    print("Thanks for using the program")
+                    break
+                
+                if self.username in self.database.keys():
+                    if self.password == self.database[self.username][0]:
+                        self.services()
+                        break
+                    elif self.password != self.database[self.username][0]:
+                        print("Incorrect Credentials")
                         break
                 else:
-                    print("Incorrect credentials, try again")
+                    user_answer = input("account doesn't exist, Would you like to create a new account (y/n): ").lower()
+                    if user_answer == 'y':
+                        self.create_account()
+                    else:
+                        print("Thanks for using the program")
+                        break
+                
+    
+    def create_account(self):
+        while True:
+            new_username = input("Provide your username: ")
+            new_password = input("Provide your password: ")
+            new_balance = int(input("Provide your balance: "))
+            if new_balance >= 0 and new_username not in self.database.keys():
+                self.database[new_username] = [new_password, new_balance]
+                print("Account Created successfully.")
+                break
             else:
-                answer = input("User not found, would you like to create an account? (y/n) : ").lower()
-                if answer == 'y':
-                    new_username = input("Enter your Username: ")
-                    new_password = input("Enter your password: ")
-                    new_balance = int(input("Enter your Balance: "))
-                    self.database[new_username] = [new_password, new_balance]
-                    print("Account added successfully")
-                else:
-                    print("Thank your for using the program")
-                    break
+                print("Please provide valid input.")
+            
+    def services(self):
+        while True:
+            print("Choose a service :")
+            print("Check balance (b)")
+            print("Deposit (d)")
+            print("Withdraw (w)")
+            answer = input().lower()
+            if answer == 'b':
+                self.balance_check()
+            elif answer == 'd':
+                self.deposit()
+            elif answer == 'w':
+                self.withdraw()
+            elif answer == '':
+                print("Logging out")
+                return
+            else:
+                print("Provide valid input.")
     
     def deposit(self):
-        user_answer = int(input("How much would you like to deposit into your account? : "))
-        self.database[self.username][1] += user_answer
-        
+        while True:
+            try:
+                user_answer = int(input("How much would you like to deposit into your account? : "))
+                self.database[self.username][1] += user_answer
+                break
+            except ValueError:
+                print("Provide valid input")
+                
     def withdraw(self):
-        user_answer = int(input("How much would you like withdraw from your account? : "))
-        if self.database[self.username][1] - user_answer < 0:
-            print("insufficient funds")
-        else:
-            self.database[self.username][1] -= user_answer
-        
+        while True:
+            try:
+                user_answer = int(input("How much would you like withdraw from your account? : "))
+                if self.database[self.username][1] - user_answer < 0:
+                    print("insufficient funds")
+                else:
+                    self.database[self.username][1] -= user_answer
+                    break
+            except ValueError:
+                print("Provide valid Input.")
+            
     def balance_check(self):
         print(f"Account Balance: {self.database[self.username][1]}")
     
