e8d370890c5423d71c01071b58ee301ed813b814
Omar Anas <elmahdyomar32@gmail.com>
2026-02-07 10:33:08 +0300

    startup def completed

--- Local_Url_Shortener/Local_Url_Shortener.py
+++ Local_Url_Shortener/Local_Url_Shortener.py
@@ -1,3 +1,27 @@
+import random
+import string
 class Shortener:
     def __init__(self):
-        pass
+        self.database = {}
+        self.startup()
+    
+    def startup(self):
+        print('-' * 47)
+        print('- Shorten URL (s) | Expand URL (e) | exit (q) -')
+        print('-' * 47)
+        while True:
+            user_input = input("which service do you prefer?: ").lower()
+            if user_input == 's':
+                self.shorten()
+            elif user_input == 'e':
+                self.expand()
+            else:
+                print("Provide valid input.")
+
+    def shorten(self):
+        user_input = input("Enter your URL: ")
+
+    
+    def expand(self):
+        print("EXPAND DEF")
+Shortener()
