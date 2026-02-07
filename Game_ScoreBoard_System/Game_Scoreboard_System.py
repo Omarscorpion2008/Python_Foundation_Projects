4345ca1955fdd00dc8e243d15706b42dcc40de78
Omar Anas <elmahdyomar32@gmail.com>
2026-02-07 11:36:46 +0300

    Game Scoreboard System project start

---
+++ Game_ScoreBoard_System/Game_Scoreboard_System.py
@@ -0,0 +1,27 @@
+class Scoreboard:
+    def __init__(self):
+        self.database = {}
+
+        self.data_loader()
+        self.menu()
+        self.data_saver()
+    
+    def data_loader(self):
+        pass
+
+    def menu(self):
+        pass
+
+    def addition(self):
+        pass
+    
+    def update(self):
+        pass
+
+    def view(self):
+        pass
+
+    def data_saver(self):
+        pass
+
+Scoreboard()
