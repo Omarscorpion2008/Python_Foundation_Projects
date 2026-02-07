f12f71999fd13784ec085dca839ee8f169c148d3
Omar Anas <elmahdyomar32@gmail.com>
2026-02-07 13:08:18 +0300

    menu def completed

--- Game_ScoreBoard_System/Game_Scoreboard_System.py
+++ Game_ScoreBoard_System/Game_Scoreboard_System.py
@@ -7,10 +7,35 @@ class Scoreboard:
         self.data_saver()
     
     def data_loader(self):
-        pass
+        try:
+            with open(r'Game_ScoreBoard_System\database.csv','r', encoding='utf-8') as file:
+                self.content = file.read()
+                self.lines_list = self.content.splitlines()
+                
+                for line in self.lines_list:
+                    items = line.split(',')
+                    self.database[items[0]] = int(items[-1])
+        except FileNotFoundError:
+            pass
 
     def menu(self):
-        pass
+        while True:
+            print('-' * 71)
+            print(' ' * 26 + '- Game Scoreboard -')
+            print('-' * 71)
+            print(' Add Player (a) | Update Player Score (u) | View Scores (v) | exit (q)')
+            user_input = input("What service do you prefer?: ").lower()
+            if user_input == 'a':
+                self.addition()
+            elif user_input == 'u':
+                self.update()
+            elif user_input == 'v':
+                self.view()
+            elif user_input == 'q':
+                print("Thanks for using the program.")
+                break
+            else:
+                print("Please provide valid input.")
 
     def addition(self):
         pass
