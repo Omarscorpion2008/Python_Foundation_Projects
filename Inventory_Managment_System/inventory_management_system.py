de3094d7858d3906ac8a7e01af7ca706ddeb6a29
Omar Anas <elmahdyomar32@gmail.com>
2026-04-25 23:10:47 +0300

    stock update and selling stock def created.

--- Inventory_Managment_System/inventory_management_system.py
+++ Inventory_Managment_System/inventory_management_system.py
@@ -28,7 +28,7 @@ class Inventory:
         
         for line in database_lines[1:]:
             items = line.split(',')
-            self.data[items[0]] = items[1:]
+            self.data[items[0]] = list(map(float,items[1:]))
 
         print(self.data)
 
@@ -42,7 +42,7 @@ class Inventory:
     def product_addition(self):
 
         while True:
-            product_name = input("Enter the product's name: ")
+            product_name = input("Enter the product's name: ").lower()
             if product_name != '':
                 break
             else:
@@ -72,16 +72,64 @@ class Inventory:
         if product_name in self.data.keys():
             print("Product already exists in database.")
         else:
-            self.data[product_name] = [quantity_number,product_price]
+            self.data[product_name] = [quantity_number,product_price,0]
             print("Product added successfully")
         
         print(self.data)
 
     def stock_update(self):
-        pass
+        while True:
+            product = input("Which product stock are you modifying?: ")
+            if product != '':
+                break
 
+        while True:
+            restock_correction = input("Addition (a) | Subtraction (s): ").lower()
+            if restock_correction == 'a':
+                while True:
+                    number = int(input("Enter the number to be added: "))
+                    if number > 0:
+                        self.data[product][0] += number
+                        break
+                    else:
+                        print("please enter a valid number")
+                        break
+                break
+
+            elif restock_correction == 's':
+                while True:
+                    number = int(input("Enter the number to be subtracted: "))
+                    if number > 0:
+                        check = self.data[product][0] - number
+                        if check < 0:
+                            print("can't make stock go into negative.")
+                        else:
+                            self.data[product][0] -= number
+                        break
+                    else:
+                        print("Please enter a valid number")
+                        break
+                break
+    
     def sell_stock(self):
-        pass
+        while True:
+            product_name = input("Enter the product's name you are wishing to sell: ")
+            if product_name != '':
+                if product_name in self.data.keys():
+                    break
+        
+        while True:
+            number = int(input("How many pieces are you selling: "))
+            if ( self.data[product_name][0] - number ) >= 0:
+                self.data[product_name][0] -= number
+                revenue = number * self.data[product_name][1]
+                self.data[product_name][2] += revenue
+                break
+            else:
+                print("Please enter a valid input.")
+                break
+
+        print(self.data)
 
     def inventory_value(self):
         pass
